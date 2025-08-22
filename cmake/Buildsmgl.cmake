592335b9a4a93bfb1cfd7eaeae419149380f2d12
Seth Parker <csparker247@gmail.com>
2025-08-22 10:43:40 -0400

    cmake: Fix build issues

--- cmake/Buildsmgl.cmake
+++ cmake/Buildsmgl.cmake
@@ -1,10 +1,16 @@
+find_package(nlohmann_json 3.9.1)
+if(nlohmann_json_FOUND)
+    option(RT_BUILD_JSON "Build in-source JSON library" off)
+else()
+    option(RT_BUILD_JSON "Build in-source JSON library" on)
+endif()
 FetchContent_Declare(
     smgl
     GIT_REPOSITORY https://gitlab.com/educelab/smgl.git
     GIT_TAG v0.10.1
     EXCLUDE_FROM_ALL
 )
-set(SMGL_BUILD_JSON ON CACHE INTERNAL "")
+set(SMGL_BUILD_JSON ${RT_BUILD_JSON} CACHE INTERNAL "")
 set(SMGL_USE_BOOSTFS ${RT_USE_BOOSTFS} CACHE INTERNAL "")
 set(SMGL_BUILD_TESTS OFF CACHE INTERNAL "")
 set(SMGL_BUILD_DOCS OFF CACHE INTERNAL "")
